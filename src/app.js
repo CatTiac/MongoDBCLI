2b6ecf102462d56216e4565e869f5946ebb4a823
Cait J <95374937+CatTiac@users.noreply.github.com>
2022-02-23 12:53:03 +0000

    updated to work with user input

--- src/app.js
+++ src/app.js
@@ -8,34 +8,29 @@ const app = async (yargsObj) => {
     const collection = await connection();
     try {
         if (yargsObj.add) {
-            const movie = new Movie(yargsObj.title, yargsObj.actor);
+            const movie = new Movie(yargsObj.title, yargsObj.actor, yargsObj.review, yargsObj.rating);
             console.log(await movie.add(collection));
             //Above - take movie info, add it to mongodb database and console log a success message
 
         } else if (yargsObj.list) {
-            const movie = new Movie(yargsObj.title, yargsObj.actor);
+            const movie = new Movie(yargsObj.title, yargsObj.actor, yargsObj.review, yargsObj.rating);
             console.log(await movie.list(collection))
             //Above - list all movies in database
 
 
         } else if (yargsObj.update) {
-            //Below - filter to update all movies with title "Spiderman"
-            const filter = { title: "Spiderman" };
-            //Below - Updates matching doc (with title"Spiderman"(above)) with comment
-            //$set - Sets the value of a field in a document
-            const updateDoc = {
-                $set: {
-                    review: "Great film!",
-                },
-            };
-            const result = await collection.updateMany(filter, updateDoc);
-            console.log(`Updated ${result.modifiedCount} movie(s)`);
+            const movie = new Movie(yargsObj.title, yargsObj.actor, yargsObj.review, yargsObj.rating);
+            console.log(await movie.update(collection));
+            
 
         } else if (yargsObj.delete) {
+            const movie = new Movie(yargsObj.title, yargsObj.actor, yargsObj.review, yargsObj.rating);
+            console.log(await movie.delete(collection));
             //Below - deletes item based on title "Matilda"
-            const filter = { title: "Matilda" };
-            const result = await collection.deleteMany(filter);
-            console.log("Deleted " + result.deletedCount + " movie(s)");
+            // const filter = { title: "Matilda" };
+            // const result = await collection.deleteMany(filter);
+            // console.log("Deleted " + result.deletedCount + " movie(s) - " + filter.title);
+
 
         } else {
             console.log("Incorrect command")
